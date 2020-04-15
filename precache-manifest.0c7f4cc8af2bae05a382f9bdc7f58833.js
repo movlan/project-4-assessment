9bacb5f49d105b7e0b9b4c37e1a53a9870dbcb03
Bahram Movlanov <movlan@users.noreply.github.com>
2020-04-15 14:56:28 -0600

    Updates

---
+++ precache-manifest.0c7f4cc8af2bae05a382f9bdc7f58833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfc4fbf8b9c2ac317a21ca49eff9366",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "4c89ad39399d9c698868",
+    "url": "/project-4-assessment/static/css/main.edcb1925.chunk.css"
+  },
+  {
+    "revision": "bf9faed5984ec60f7781",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c89ad39399d9c698868",
+    "url": "/project-4-assessment/static/js/main.fff367fb.chunk.js"
+  },
+  {
+    "revision": "bb8b828653ad1446ab40",
+    "url": "/project-4-assessment/static/js/runtime-main.58f97c80.js"
+  }
+]);
